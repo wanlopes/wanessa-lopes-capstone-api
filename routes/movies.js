d8be64f03b9791bfa2078a1cb246bba40f277d97
Wanessa Lopes <wanessaalopes@gmail.com>
2024-03-07 21:58:37 -0700

    Set validation for user info

--- routes/movies.js
+++ routes/movies.js
@@ -3,12 +3,12 @@ const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
-var cors = require("cors");
 
-const url = "https://api.themoviedb.org/3/account/null";
-const options = { method: "GET", headers: { accept: "application/json" } };
 
-fetch(url, options)
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error("error:" + err));
+const {
+  validateItemInput,
+  validateMiddleware,
+} = require("../../middleware/validationMiddleware");
+
+const knex = require("knex")(require("../../knexfile"));
+
