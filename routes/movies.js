e7961068830800ffe02a7bacd0da0f823d91c22f
Wanessa Lopes <wanessaalopes@gmail.com>
2024-03-09 16:33:15 -0700

    Use retry strategie to fetch movies

--- routes/movies.js
+++ routes/movies.js
@@ -14,17 +14,7 @@ router.get("/search", async (req, res) => {
   const query = req.query.query;
   console.log(query);
   try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/search/movie`,
-      {
-        params: {
-          api_key: process.env.YOUR_TMDB_API_KEY,
-          query: query,
-          include_adult: false,
-        },
-      }
-    );
-
+    const response = await fetchMoviesWithRetry(query);
     const movies = response.data.results.map((movie) => ({
       id: movie.id,
       title: movie.title,
@@ -38,5 +28,31 @@ router.get("/search", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+async function fetchMoviesWithRetry(query, retries = 20, delay = 10) {
+  for (let i = 0; i < retries; i++) {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/search/movie`,
+        {
+          params: {
+            api_key: process.env.YOUR_TMDB_API_KEY,
+            query: query,
+            include_adult: false,
+          },
+        }
+      );
+      if (response.status != 200 && response.status != 404) {
+        console.log(response);
+        throw new Error(`Request failed`);
+      }
+      return response; // If successful, return data
+    } catch (error) {
+      console.error(`Attempt ${i + 1} failed:`, error.message);
+      await new Promise((resolve) => setTimeout(resolve, delay)); // Wait for specified delay
+    }
+  }
+  throw new Error(`Failed to fetch data after ${retries} attempts`);
+}
 
 module.exports = router;
